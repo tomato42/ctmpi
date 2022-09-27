80f4bc847c510dd3992ad67a4545f00feaac3ee8
Hubert Kario <hkario@redhat.com>
2022-09-27 15:03:37 +0200

    tests for modulo operation

--- harness.c
+++ harness.c
@@ -10,13 +10,14 @@
 enum operation {Add, Sub, Mul, Mod, Rshift1, Cselect};
 
 void help(char *name) {
-    printf("Usage: %s -i file -o file -n num [-a|-s|-m|-o]\n", name);
+    printf("Usage: %s -i file -o file -n num [-a|-s|-m|-o|-d -2 num]\n", name);
     printf("\n");
     printf(" -i file  input file\n");
     printf(" -o file  output file to write timing data to\n");
     printf(" -n num   size of input numbers in bytes (needs to be a "
            "multiple\n");
     printf("          of word size: 32bit or 64 bit)\n");
+    printf(" -2 num   size of output number in bytes (modulo only)\n");
     printf(" -a       perform addition test (default)\n");
     printf(" -s       perform subtraction test\n");
     printf(" -m       perform multiplication test\n");
@@ -457,6 +458,145 @@ fail:
     return ret;
 }
 
+int time_mod(size_t numb, size_t nummod, int in_file, int out_file) {
+    char *buf=NULL;
+    char prn_buf[1024];
+    limb_t *a=NULL, *b=NULL;
+    limb_t *rem=NULL, *exp_rem=NULL;
+    limb_t *tmp=NULL;
+    int ret = 0;
+    ssize_t r_ret;
+    size_t limb_count = numb / LIMB_BYTE_SIZE;
+    size_t mod_limb_count = nummod / LIMB_BYTE_SIZE;
+
+    uint32_t time_before_high = 0, time_before_low = 0, time_after_high = 0,
+             time_after_low = 0;
+
+    r_ret = write(out_file, "mod_times\n", 10);
+    if (r_ret != 10) {
+        fprintf(stderr, "Writing header to output file failed\n");
+        ret = 1;
+        goto fail;
+    }
+
+    buf = malloc(sizeof(char) * ((numb > nummod) ? numb : nummod));
+    if (buf == NULL) {
+        fprintf(stderr, "malloc fail (buf)\n");
+        ret = 1;
+        goto fail;
+    }
+    a = malloc(sizeof(limb_t) * limb_count);
+    if (a == NULL) {
+        fprintf(stderr, "malloc fail (a)\n");
+        ret = 1;
+        goto fail;
+    }
+    b = malloc(sizeof(limb_t) * mod_limb_count);
+    if (b == NULL) {
+        fprintf(stderr, "malloc fail (b)\n");
+        ret = 1;
+        goto fail;
+    }
+    rem = malloc(sizeof(limb_t) * mod_limb_count);
+    if (rem == NULL) {
+        fprintf(stderr, "malloc fail (rem)\n");
+        ret = 1;
+        goto fail;
+    }
+    exp_rem = malloc(sizeof(limb_t) * mod_limb_count);
+    if (exp_rem == NULL) {
+        fprintf(stderr, "malloc fail (exp_rem)\n");
+        ret = 1;
+        goto fail;
+    }
+    tmp = malloc(sizeof(limb_t) * mod_limb_numb(limb_count, mod_limb_count));
+    if (tmp == NULL) {
+        fprintf(stderr, "malloc fail (tmp)\n");
+        ret = 1;
+        goto fail;
+    }
+
+    while ((r_ret = read(in_file, buf, numb)) > 0) {
+        if (r_ret != numb) {
+            fprintf(stderr, "read less data than expected (truncated file?)\n");
+            ret = 1;
+            goto fail;
+        }
+        be_buf_to_limb_t(buf, a, limb_count);
+
+        r_ret = read(in_file, buf, nummod);
+        if (r_ret <= 0) {
+            fprintf(stderr, "can't read (b) value from input file (truncated "
+                    "file?)\n");
+            ret = 1;
+            goto fail;
+        }
+        be_buf_to_limb_t(buf, b, mod_limb_count);
+
+        r_ret = read(in_file, buf, nummod);
+        if (r_ret <= 0) {
+            fprintf(stderr, "can't read (exp_rem) value from input file "
+                    "(truncated file?)\n");
+            ret = 1;
+            goto fail;
+        }
+        be_buf_to_limb_t(buf, exp_rem, mod_limb_count);
+
+        asm volatile (
+            "CPUID\n\t"
+            "RDTSC\n\t"
+            "mov %%edx, %0\n\t"
+            "mov %%eax, %1\n\t" : "=r" (time_before_high),
+            "=r" (time_before_low)::
+            "%rax", "%rbx", "%rcx", "%rdx");
+
+        mod(rem, a, limb_count, b, mod_limb_count, tmp);
+
+        asm volatile (
+            "RDTSCP\n\t"
+            "mov %%edx, %0\n\t"
+            "mov %%eax, %1\n\t"
+            "CPUID\n\t": "=r" (time_after_high),
+            "=r" (time_after_low)::
+            "%rax", "%rbx", "%rcx", "%rdx");
+
+        if (memcmp(rem, exp_rem, nummod) != 0) {
+            fprintf(stderr, "mod() result incorrect\n");
+            ret = 1;
+            goto fail;
+        }
+
+        r_ret = snprintf(prn_buf, 1024, "%d\n",
+            (uint32_t)(((uint64_t)time_after_high<<32 | time_after_low)-
+            ((uint64_t)time_before_high<<32 | time_before_low)));
+        if (r_ret >= 1024) {
+            fprintf(stderr, "Unexpected snprintf output\n");
+            ret = 1;
+            goto fail;
+        }
+        r_ret = write(out_file, prn_buf, r_ret);
+        if (r_ret <= 0) {
+            fprintf(stderr, "Write error\n");
+            ret = 1;
+            goto fail;
+        }
+    }
+
+    if (r_ret < 0) {
+        fprintf(stderr, "Reading from input file failed\n");
+        ret = 1;
+        goto fail;
+    }
+
+fail:
+    free(buf);
+    free(a);
+    free(b);
+    free(rem);
+    free(exp_rem);
+    free(tmp);
+    return ret;
+}
 
 int main(int argc, char** argv) {
     int ret = 0;
@@ -465,9 +605,10 @@ int main(int argc, char** argv) {
     char *out_name = NULL;
     int in_file, out_file;
     size_t numb = 0;
+    size_t nummod = 0;
     enum operation oper = Add;
 
-    while ((opt = getopt(argc, argv, "i:o:n:asmdh")) != -1) {
+    while ((opt = getopt(argc, argv, "i:o:n:2:asmdh")) != -1) {
         switch(opt) {
             case 'i':
                 in_name = optarg;
@@ -478,6 +619,8 @@ int main(int argc, char** argv) {
             case 'n':
                 sscanf(optarg, "%zi", &numb);
                 break;
+            case '2':
+                sscanf(optarg, "%zi", &nummod);
             case 'a':
                 oper = Add;
                 break;
@@ -508,6 +651,11 @@ int main(int argc, char** argv) {
         help(argv[0]);
         exit(1);
     }
+    if (oper == Mod && nummod == 0) {
+        fprintf(stderr, "Specify the size of output for modulo operation\n");
+        help(argv[0]);
+        exit(1);
+    }
 
     in_file = open(in_name, O_RDONLY);
     if (in_file == -1) {
@@ -530,6 +678,9 @@ int main(int argc, char** argv) {
             break;
         case Mul:
             ret = time_mul(numb, in_file, out_file);
+            break;
+        case Mod:
+            ret = time_mod(numb, nummod, in_file, out_file);
             break;
         default:
             fprintf(stderr, "Unknown operation: %d\n", oper);
--- test_data_gen.py
+++ test_data_gen.py
@@ -12,16 +12,20 @@ def help_msg():
     print("           systems. Default: 8")
     print(" -n num    Number of machine words to use for generated numbers")
     print("           Default: 32 (i.e. 2048 bit on 64-bit machine)")
+    print(" -2 num    Number of machine words to use for mod operation output")
+    print("           Default: 32")
     print(" -l file   Name of output log file, \"log.csv\" by default")
     print(" -o file   Name of output file with generated data.")
     print("           \"data.bin\" by default")
     print(" --add     Generate data for addition tests (default)")
     print(" --sub     Generate data for subtraction tests")
     print(" --mul     Generate data for multiplication tests")
+    print(" --mod     Generate data for modulo tests")
     print(" -h        This message")
 
 limb_size = 8
 nlimb = 32  # operate on 2048 bit numbers
+mod_limb = 32
 
 N = 100000
 log_out = "log.csv"
@@ -33,7 +37,7 @@ data_file = open(data_out, "wb")
 
 argv = sys.argv[1:]
 
-opts, args = getopt.getopt(argv, "N:b:n:l:o:h", ["add", "sub", "mul"])
+opts, args = getopt.getopt(argv, "N:b:n:2:l:o:h", ["add", "sub", "mul", "mod"])
 for opt, arg in opts:
     if opt == "-h":
         help_msg()
@@ -44,6 +48,8 @@ for opt, arg in opts:
         limb_size = int(arg)
     elif opt == "-n":
         nlimb = int(arg)
+    elif opt == "-2":
+        modlimb = int(arg)
     elif opt == "-l":
         log_out = arg
     elif opt == "-o":
@@ -54,6 +60,8 @@ for opt, arg in opts:
         oper = "sub"
     elif opt == "--mul":
         oper = "mul"
+    elif opt == "--mod":
+        oper = "mod"
     else:
         print("Unrecognised option: {0}".format(opt))
         help_msg()
@@ -65,7 +73,10 @@ if args:
     sys.exit(1)
 
 # make a modulus the count of numbers that can be represented in this many bits
+# for the input
 mod = 2**(8*limb_size*nlimb)
+# for the mod op output
+mod_mod = 2**(8*limb_size*mod_limb)
 
 probes = [
         ("null-null", (0, 0)),
@@ -89,6 +100,26 @@ probes = [
                        2**(8*limb_size*nlimb)-1-(2**(8*limb_size*24)-1))),
         ]
 
+mod_probes = [
+        ("null-mod-1", (0, mod_mod-1)),
+        ("one-mod-1", (1, mod_mod-1)),
+        ("qword-mod-1", (2**(8*limb_size)-1, mod_mod-1)),
+        ("mod-2-mod-1", (mod_mod-2, mod_mod-1)),
+        ("max-mod-1", (mod-1, mod_mod-1)),
+        ("mod-1-sq-mod-1", (((mod_mod-1)**2)%(mod-1), mod_mod-1)),
+        ("null-mod>>limb", (0, mod_mod >> (limb_size * 8))),
+        ("null-mod-1>>limb", (0, (mod_mod - 1) >> (limb_size * 8))),
+        ("null-mod>>2*limb", (0, mod_mod >> (limb_size * 8 * 2))),
+        ("null-mod-1>>2*limb", (0, (mod_mod - 1) >> (limb_size * 8 * 2))),
+        ("max-mod>>limb", (mod-1, mod_mod >> (limb_size * 8))),
+        ("max-mod-1>>limb", (mod-1, (mod_mod - 1) >> (limb_size * 8))),
+        ("max-mod>>2*limb", (mod-1, mod_mod >> (limb_size * 8 * 2))),
+        ("max-mod-1>>2*limb", (mod-1, (mod_mod - 1) >> (limb_size * 8 * 2))),
+        ]
+
+if oper == "mod":
+    probes = mod_probes
+
 log_file.write(",".join(name for name, _ in probes))
 log_file.write("\n")
 
@@ -99,14 +130,22 @@ for _ in range(N):
     order = random.sample(range(len(probes)), len(probes))
     for i, pos in enumerate(order):
         val_a, val_b = probes[pos][1]
+        # write inputs to the file
         assert val_a < mod
-        assert val_b < mod
         data_file.write(val_a.to_bytes(length=limb_size*nlimb, byteorder="big"))
-        data_file.write(val_b.to_bytes(length=limb_size*nlimb, byteorder="big"))
+        if oper == "mod":
+            assert val_b < mod_mod
+            data_file.write(val_b.to_bytes(length=limb_size*mod_limb, byteorder="big"))
+        else:
+            assert val_b < mod
+            data_file.write(val_b.to_bytes(length=limb_size*nlimb, byteorder="big"))
+        # write expected output to the file
         if oper == "add":
             data_file.write(((val_a + val_b) & (mod-1)).to_bytes(length=limb_size*nlimb, byteorder="big"))
         elif oper == "mul":
             data_file.write((val_a * val_b).to_bytes(length=limb_size*nlimb*2, byteorder="big"))
+        elif oper == "mod":
+            data_file.write((val_a % val_b).to_bytes(length=limb_size*mod_limb, byteorder="big"))
         else:
             assert oper == "sub"
             data_file.write(((val_a - val_b) & (mod-1)).to_bytes(length=limb_size*nlimb, byteorder="big"))
